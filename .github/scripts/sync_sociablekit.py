df19bf3e7733b81257e9ca0b7adbb71add15c8f7
Dave Rendon <1934250+daveRendon@users.noreply.github.com>
2025-09-18 18:39:57 -0600

    Update Chrome WebDriver initialization method

--- .github/scripts/sync_sociablekit.py
+++ .github/scripts/sync_sociablekit.py
@@ -46,7 +46,7 @@ def main() -> int:
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+    driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=chrome_options)
     driver.set_page_load_timeout(60)
 
     try:
